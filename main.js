fd669b6670a13d61907674ed6f1cec9c9bc30ca8
SydVic <fabrizio.bocca27@gmail.com>
2022-05-03 14:10:24 +0200

    scaffolding

---
+++ main.js
@@ -0,0 +1,58 @@
+const posts = [
+    {
+        "id": 1,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "author": {
+            "name": "Phil Mangione",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 80,
+        "created": "2021-06-25"
+    },
+    {
+        "id": 2,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=154",
+        "author": {
+            "name": "April O'Neil",
+            "image": "https://unsplash.it/300/300?image=31"
+        },
+        "likes": 15,
+        "created": "2021-06-25"
+    },
+    {
+        "id": 3,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=36",
+        "author": {
+            "name": "Stephan Staruss",
+            "image": "https://unsplash.it/300/300?image=74"
+        },
+        "likes": 36,
+        "created": "2021-06-25"
+    },
+    {
+        "id": 4,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=58",
+        "author": {
+            "name": "Mike Dellavo",
+            "image": "https://unsplash.it/300/300?image=82"
+        },
+        "likes": 125,
+        "created": "2021-06-25"
+    },
+    {
+        "id": 5,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=99",
+        "author": {
+            "name": "Gino Pino",
+            "image": "https://unsplash.it/300/300?image=41"
+        },
+        "likes": 27,
+        "created": "2021-06-25"
+    },
+];
+
